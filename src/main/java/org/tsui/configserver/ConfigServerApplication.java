3df8e3ccb774680a0c332f60551d7f6095db5b9d
cuiyunhong <smartcyh@hotmail.com>
2018-11-27 23:27:19 +0800

    Initial commit

---
+++ src/main/java/org/tsui/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package org.tsui.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author cuiyunhong.
+ * Created by cuiyunhong on 2018/11/27.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
